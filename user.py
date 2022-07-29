62965ecb5fa595267879e06803bc5af56d9b6d23
cwadven <cwadven@naver.com>
2022-07-29 23:28:32 +0900

    chore: method added
    - 필드 추가

--- user.py
+++ user.py
@@ -3,3 +3,7 @@ class User(object):
         self.name = name
         self.age = age
         self.sex = sex
+        self.sdfsdf = 1234
+
+    def hello(self):
+        print(f'저는 {self.name} 입니다. 안녕하세요')
